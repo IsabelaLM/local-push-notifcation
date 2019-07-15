4536bc70a5dada8c63971bed98348104b76d0f7e
Isabela <isabela.dev@live.com>
2019-07-15 14:43:29 -0300

    chore: basic initial project structure

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// import Routes from './routes';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>oie</Text>
+    </View>
+  );
+}
